eab3492d3718d9fcf2d78367ccd992b980063033
pritalb <pritalbamnodkar@gmail.com>
2022-12-13 14:27:03 +0530

    implemented checkWin

--- script.js
+++ script.js
@@ -24,6 +24,51 @@ function playGame() {
             return availableTiles.includes(tileID);
         }
 
+        const checkDraw = () => {
+            return availableTiles.length === 0;
+        }
+
+        const checkDiagonalWin = (mark) => {
+            let win = gameboard[1][1] === mark;
+            
+            if (!win) {
+                return false;
+            }
+
+            return  (gameboard[0][0] === gameboard[1][1] && gameboard[1][1] === gameboard[2][2]) ||
+                    (gameboard[0][2] === gameboard[1][1] && gameboard[1][1] === gameboard[2][0]);
+        }
+
+        const checkColumnWin = (mark) => {
+            let win = false;
+
+            for (let i = 0; i < 3; i++) {
+                let columnFirstElement = gameboard[0][i];
+                win = (columnFirstElement === mark && 
+                        columnFirstElement === gameboard[1][i] && 
+                        gameboard[1][i] === gameboard[2][i]) || win;
+            }
+
+            return win;
+        }
+
+        const checkRowWin = (mark) => {
+            let win = false;
+
+            for(let i = 0; i < 3; i++) {
+                let row = gameboard[i]
+                console.log(row);
+
+                win = (row[0] === mark && row[0] === row[1] && row[1] === row[2]) || win;
+            }
+
+            return win;
+        }
+
+        const checkWin = (mark) => {
+            return checkColumnWin(mark) || checkRowWin(mark) || checkDiagonalWin(mark);
+        }
+
         const markTile = (tileID, mark) => {
             let row = Number(tileID[0]);
             let col = Number(tileID[1]);
@@ -38,7 +83,7 @@ function playGame() {
             }
         }
 
-        return {getGameboard, getAvailableTiles, markTile}
+        return {getGameboard, getAvailableTiles, checkDraw, checkWin, markTile}
     })();
 
     const playerFactory = (mark) => {
@@ -48,12 +93,13 @@ function playGame() {
     console.log(gameBoard.getGameboard());
     console.log(gameBoard.getAvailableTiles());
 
-    gameBoard.markTile('11', 'X');
-    gameBoard.markTile('01', '0');
-    gameBoard.markTile('01', 'X');
+    // console.log(gameBoard.checkDraw());
+    // gameBoard.markTile('11', 'X');
+    // gameBoard.markTile('01', '0');
+    // gameBoard.markTile('01', 'X');
 
-    console.log(gameBoard.getGameboard());
-    console.log(gameBoard.getAvailableTiles());
+    // console.log(gameBoard.getGameboard());
+    // console.log(gameBoard.getAvailableTiles());
 }
 
 function main() {
